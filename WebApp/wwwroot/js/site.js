239ce6bfb719416e8d20fa3b078daa6ba81fd59e
Jakob Hjalmarsson <jakob.hjalmarsson@learnet.se>
2025-03-28 22:27:28 +0100

    Added settings dropdown

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,1 +1,8 @@
-﻿
+﻿const settingsDropdown = document.querySelector('.settings-dropdown')
+const settingsButton = document.querySelector('#settings')
+
+function openDropdown() {
+    settingsDropdown.classList.toggle('hidden')
+}
+
+settingsButton.addEventListener('click', openDropdown)
