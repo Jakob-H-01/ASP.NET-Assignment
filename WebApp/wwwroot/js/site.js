e7f23ddac01a7276101cb511ebcd7df9d24a1bdf
Jakob Hjalmarsson <jakob.hjalmarsson@learnet.se>
2025-04-03 17:12:04 +0200

    Added project options dropdown and modified the js slightly

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -5,16 +5,24 @@ toggleButtons.forEach(button => {
     views.push(view)
 })
 
-function toggleView(targetView) {
+function toggle(element, targetView) {
     views.forEach(view => {
         if (view.id !== targetView.id) {
             view.classList.add('hidden')
         }
     })
+
+    toggleButtons.forEach(button => {
+        if (button.dataset.target !== element.dataset.target) {
+            button.classList.remove('active')
+        }
+    })
+
     targetView.classList.toggle('hidden')
+    element.classList.toggle('active')
 }
 
 toggleButtons.forEach(button => {
     const targetView = document.querySelector(button.dataset.target)
-    button.addEventListener('click', () => toggleView(targetView))
+    button.addEventListener('click', () => toggle(button, targetView))
 })
