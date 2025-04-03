46ee334ab3b363db3e7fb23be704f959c48fee63
Jakob Hjalmarsson <jakob.hjalmarsson@learnet.se>
2025-04-03 19:56:20 +0200

    Made the add project/member buttons open their respective modal and also made it possible to close them with another button

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -4,8 +4,9 @@ toggleButtons.forEach(button => {
     const view = document.querySelector(button.dataset.target)
     views.push(view)
 })
+const modalButtons = document.querySelectorAll('[data-type="modal"]')
 
-function toggle(element, targetView) {
+function toggleDropdown(element, targetView) {
     views.forEach(view => {
         if (view.id !== targetView.id) {
             view.classList.add('hidden')
@@ -22,7 +23,15 @@ function toggle(element, targetView) {
     element.classList.toggle('active')
 }
 
+function toggleModal(modal) {
+    modal.classList.toggle('hidden')
+}
+
 toggleButtons.forEach(button => {
     const targetView = document.querySelector(button.dataset.target)
-    button.addEventListener('click', () => toggle(button, targetView))
+    button.addEventListener('click', () => toggleDropdown(button, targetView))
+})
+modalButtons.forEach(button => {
+    const targetModal = document.querySelector(button.dataset.target)
+    button.addEventListener('click', () => toggleModal(targetModal))
 })
