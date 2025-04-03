de7448b39af5bfcb6ff19459794cd29f00e6231b
Jakob Hjalmarsson <jakob.hjalmarsson@learnet.se>
2025-04-03 14:26:27 +0200

    Added notifications dropdown and made improvements to the css and js

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,8 +1,20 @@
-﻿const settingsDropdown = document.querySelector('.settings-dropdown')
-const settingsButton = document.querySelector('#settings')
+﻿const toggleButtons = document.querySelectorAll('[data-type="toggle"]')
+const views = []
+toggleButtons.forEach(button => {
+    const view = document.querySelector(button.dataset.target)
+    views.push(view)
+})
 
-function openDropdown() {
-    settingsDropdown.classList.toggle('hidden')
+function toggleView(targetView) {
+    views.forEach(view => {
+        if (view.id !== targetView.id) {
+            view.classList.add('hidden')
+        }
+    })
+    targetView.classList.toggle('hidden')
 }
 
-settingsButton.addEventListener('click', openDropdown)
+toggleButtons.forEach(button => {
+    const targetView = document.querySelector(button.dataset.target)
+    button.addEventListener('click', () => toggleView(targetView))
+})
