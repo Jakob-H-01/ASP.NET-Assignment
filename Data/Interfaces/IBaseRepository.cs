a37e69dfe50aaecedf4f7c37be1436446c8a55a7
Jakob Hjalmarsson <jakob.hjalmarsson@learnet.se>
2025-04-09 23:53:31 +0200

    Created a base repository with dynamic mapping and better responses and created a domain project

---
+++ Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using Data.Models;
+
+namespace Data.Interfaces;
+
+public interface IBaseRepository<TEntity, TModel> where TEntity : class
+{
+    Task<RepositoryResult<bool>> AddAsync(TEntity entity);
+    Task<RepositoryResult<bool>> DeleteAsync(TEntity entity);
+    Task<RepositoryResult<bool>> ExistsAsync(Expression<Func<TEntity, bool>> findBy);
+    Task<RepositoryResult<IEnumerable<TModel>>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortBy = null, Expression<Func<TEntity, bool>>? filterBy = null, params Expression<Func<TEntity, object>>[] include);
+    Task<RepositoryResult<TModel>> GetAsync(Expression<Func<TEntity, bool>> findBy, params Expression<Func<TEntity, object>>[] include);
+    Task<RepositoryResult<bool>> UpdateAsync(TEntity entity);
+}
---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,119 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+using Data.Contexts;
+using Data.Interfaces;
+using Data.Models;
+using Domain.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public abstract class BaseRepository<TEntity, TModel> : IBaseRepository<TEntity, TModel> where TEntity : class
+{
+    protected readonly DataContext _context;
+    protected readonly DbSet<TEntity> _table;
+
+    protected BaseRepository(DataContext context)
+    {
+        _context = context;
+        _table = _context.Set<TEntity>();
+    }
+
+    public virtual async Task<RepositoryResult<bool>> AddAsync(TEntity entity)
+    {
+        if (entity == null)
+            return new RepositoryResult<bool> { Succeeded = false, StatusCode = 400, Error = "Entity can't be null" };
+
+        try
+        {
+            _table.Add(entity);
+            await _context.SaveChangesAsync();
+            return new RepositoryResult<bool> { Succeeded = true, StatusCode = 201 };
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return new RepositoryResult<bool> { Succeeded = false, StatusCode = 500, Error = ex.Message };
+        }
+    }
+
+    public virtual async Task<RepositoryResult<IEnumerable<TModel>>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortBy = null, Expression<Func<TEntity, bool>>? filterBy = null, params Expression<Func<TEntity, object>>[] include)
+    {
+        IQueryable<TEntity> query = _table;
+
+        if (filterBy != null)
+            query = query.Where(filterBy);
+
+        if (include != null && include.Length > 0)
+            foreach (var item in include)
+                query = query.Include(item);
+
+        if (sortBy != null)
+            query = orderByDescending
+                ? query.OrderByDescending(sortBy)
+                : query.OrderBy(sortBy);
+
+        var entities = await query.ToListAsync();
+        var result = entities.Select(entity => entity.MapTo<TModel>());
+        return new RepositoryResult<IEnumerable<TModel>> { Succeeded = true, StatusCode = 200, Result = result };
+    }
+
+    public virtual async Task<RepositoryResult<TModel>> GetAsync(Expression<Func<TEntity, bool>> findBy, params Expression<Func<TEntity, object>>[] include)
+    {
+        IQueryable<TEntity> query = _table;
+
+        if (include != null && include.Length > 0)
+            foreach (var item in include)
+                query = query.Include(item);
+
+        var entity = await query.FirstOrDefaultAsync(findBy);
+        if (entity == null)
+            return new RepositoryResult<TModel> { Succeeded = false, StatusCode = 404, Error = "Entity not found" };
+
+        var result = entity.MapTo<TModel>();
+        return new RepositoryResult<TModel> { Succeeded = true, StatusCode = 200, Result = result };
+    }
+
+    public virtual async Task<RepositoryResult<bool>> UpdateAsync(TEntity entity)
+    {
+        if (entity == null)
+            return new RepositoryResult<bool> { Succeeded = false, StatusCode = 400, Error = "Entity can't be null" };
+
+        try
+        {
+            _table.Update(entity);
+            await _context.SaveChangesAsync();
+            return new RepositoryResult<bool> { Succeeded = true, StatusCode = 200 };
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return new RepositoryResult<bool> { Succeeded = false, StatusCode = 500, Error = ex.Message };
+        }
+    }
+
+    public virtual async Task<RepositoryResult<bool>> DeleteAsync(TEntity entity)
+    {
+        if (entity == null)
+            return new RepositoryResult<bool> { Succeeded = false, StatusCode = 400, Error = "Entity can't be null" };
+
+        try
+        {
+            _table.Remove(entity);
+            await _context.SaveChangesAsync();
+            return new RepositoryResult<bool> { Succeeded = true, StatusCode = 200 };
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return new RepositoryResult<bool> { Succeeded = false, StatusCode = 500, Error = ex.Message };
+        }
+    }
+
+    public virtual async Task<RepositoryResult<bool>> ExistsAsync(Expression<Func<TEntity, bool>> findBy)
+    {
+        return await _table.AnyAsync(findBy)
+            ? new RepositoryResult<bool> { Succeeded = true, StatusCode = 200 }
+            : new RepositoryResult<bool> { Succeeded = false, StatusCode = 404, Error = "Entity not found" };
+    }
+}
